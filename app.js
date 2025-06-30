b2b28daac282033360f205693ecf91f48439a98a
Abror git config --global user.name <abrorboymatov2005@gmail.com>
2025-06-30 06:14:39 +0500

    vazifalar

---
+++ app.js
@@ -0,0 +1,44 @@
+// 1. JavaScript asoslari va o'zgaruvchilar
+
+// Topshiriq 1:
+// let studentName = "Abror";
+// studentName = "Sardor";
+// console.log(studentName);
+
+
+// Topshiriq 3:
+// let a = +prompt("a o'zgaruvchini kiriting: ");
+// let b = +prompt("b o'zgaruvchini kiriting: ");
+// [a, b] = [b, a]
+// console.log(a, b);
+
+
+// 2.Ma'lumot turlari va typeof
+
+// Topshiriq 4;
+// let type = "salom"
+// console.log(typeof(type));
+// let typee = 1;
+// console.log(typeof(typee));
+// let typeee
+// console.log(typeee);
+// let  x=10n;
+// console.log(typeof(x));
+// const y = Symbol("ks");
+// console.log(typeof(y));
+// let n = null;
+// console.log(typeof(n));
+// const a = Symbol("class");
+// const b = Symbol("class");
+// console.log(a === b);
+
+// Topshiriq 5
+// let Tel = +prompt("Tel raqamizni kiriting")
+// let Tell = prompt("Tel raqamizni kiriting")
+// console.log(typeof(Tel));
+// console.log(typeof(Tell));
+
+//  Topshiriq 6
+let BigInt = +prompt("kjnj");
+let n = 38n()
+console.log();
